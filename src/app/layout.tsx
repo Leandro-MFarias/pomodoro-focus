7622e6f7b27ea4c33059853d48cdb8f02be4ada9
Leandro Farias <leandrorf1606@gmail.com>
2025-06-06 19:39:22 -0300

    fix: change opengraph to public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,20 @@ export const metadata: Metadata = {
   title: "Focus Lab",
   description:
     "Ambiente ideal para produtividade: organize suas tarefas, controle o tempo com o método Pomodoro e ouça músicas enquanto trabalha.",
+  openGraph: {
+    title: "Focus Lab",
+    description: "Seu app de foco com técnica Pomodoro!",
+    url: "https://pomodoro-focus-sandy.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://pomodoro-focus-sandy.vercel.app/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Logo do Focus Lab",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
